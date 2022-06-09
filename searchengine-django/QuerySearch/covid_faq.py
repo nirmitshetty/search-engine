b667014c61b23bfc628bdf8b40ef83e13808b3e3
Nirmit Shetty <nirmit123@gmail.com>
2022-06-08 23:14:13 -0700

    merging changes

--- searchengine-django/QuerySearch/covid_faq.py
+++ searchengine-django/QuerySearch/covid_faq.py
@@ -30,6 +30,11 @@ def get_video_data():
     return df
 
 
+def get_sentence_embeding(sentences):
+    preprocessed_text = sbert_model.encode(sentences)
+    return preprocessed_text
+    
+
 def get_context_values(data):
     df = get_text_data()
     predicted_context = []
@@ -49,10 +54,6 @@ def get_context_values(data):
             i += 1
 
     return predicted_context
-
-def get_sentence_embeding(sentences):
-    preprocessed_text = sbert_model.encode(sentences)
-    return preprocessed_text
 
 
 def search_covid_text_dataset(question):
@@ -92,36 +93,58 @@ def search_covid_text_dataset(question):
 
     return pred_ans
 
-def get_time_stamp(question, ans):
+def get_transcript_values(data):
+    df = get_video_data()
+    predicted_context = []
+    i = 1
+    for key, value in data.items():
+        if i < 3:
+            context = df.iloc[key]['Transcript']
+            link = df.iloc[key]['youtube_link']
+            id = df.iloc[key]['id']
+            predicted_context.append([context, link, id])
+            i += 1
+
+    return predicted_context
+
+def get_transcripts_stamp(question):
+    df = get_video_data()
+    similar_vector_values=[]
+    similar_vector={}
+    query = question
+    query_vectors = sbert_model.encode([query])
+    for index, row in df.iterrows():
+        ans = row['encoded_questions']
+        similar_vector_values.append(np.sum(cosine_similarity(ans,query_vectors)))
+        similar_vector[index]=np.sum(cosine_similarity(ans,query_vectors))
+
+    dx = np.argmax(np.array(similar_vector_values))
+    sorted_d = dict( sorted(similar_vector.items(), key=operator.itemgetter(1),reverse=True))
+    ans=get_transcript_values(sorted_d)
+    return ans
+
+
+def get_time_stamp(question):
 
     if redis_client.exists(f"bert_video_{question}"):
-
         print('fetched from cache')
         #redis_client.expire(f"bert_video_{question}",timedelta(seconds=60))
         return redis_client.get(f"bert_video_{question}")
 
-    video_df = get_video_data()
-    # print("head cdata " , df.head())
-    ans = np.array(ans)
-    ids = ans[:, 2]
+    videos = get_transcripts_stamp(question)
     video_ans = []
-    videos = video_df[video_df['id'].isin(ids)]
- 
-    for index, row in videos.iterrows():
-        
-        desc=row['Description']
-        title=row['Title']
-        id = row['id']
-        predicted_transcript = row['Transcript']
+    for index, value in enumerate(videos):
+        predicted_transcript = value[0]
         sent_dict = {}
+
         video_split = predicted_transcript.splitlines()
-        youtube_link = row['youtube_link']
+        youtube_link = value[1]
         similar_vector_values = []
         for i in range(0, len(video_split),4):
             time = video_split[i]
             sent = video_split[i+1]
             if i+3 < len(video_split):
-                sent = sent + video_split[i+3]
+                sent = sent + " " + video_split[i+3]
             sent_dict[time] = sent
         transcript_df = pd.DataFrame(list(sent_dict.items()),columns = ['timestamp','sent'])
         transcript_df['encoded_transcript'] = transcript_df['sent'].apply(lambda x: get_sentence_embeding([x]))
@@ -136,8 +159,7 @@ def get_time_stamp(question, ans):
         dx = np.argmax(np.array(similar_vector_values))
 
         predicted_time = transcript_df.iloc[dx]['timestamp']
-        #video_ans.append([predicted_time, youtube_link, id ])
-        video_ans.append([predicted_time, youtube_link, title, desc ,"video" ])
+        video_ans.append([predicted_time, youtube_link, value[2] ])
 
     video_ans=json.dumps(video_ans)
 
@@ -146,6 +168,7 @@ def get_time_stamp(question, ans):
     #redis_client.expire(f"bert_video_{question}",timedelta(seconds=60))
 
     return video_ans
+
 
 if __name__ == '__main__':
     print("main called")
